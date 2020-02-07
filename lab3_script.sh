a0d7c530a989b4f9e7bcc879eb6065c4f598d34c
Jumu3668 <boxmonkey13@outlook.com>
2020-02-07 09:46:12 -0700

    fixed script

--- lab3_script.sh
+++ lab3_script.sh
@@ -6,6 +6,13 @@ echo "File Name"
 read input1
 echo "Regex "
 read input2
+echo "Number of phone numbers containing $input2"
 grep $input2 $input1 -c
 grep 303  regex_practice.txt
+grep @ -c regex_practice.txt 
+echo "number of phone numbers starting with 303 or 720"
+let a=$(grep 303 regex_practice.txt -c)
+let b=$(grep 720 regex_practice.txt -c)
+let c=$a+$b
+echo $c
 grep @ regex_practice.txt >> email_results.txt
